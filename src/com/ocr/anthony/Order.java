7246abef9e3af678f2f64726b046a5e05bc2a1b5
cperez <chrper31@gmail.com>
2021-03-29 17:33:43 +0200

    ajouté le SWITCH au lieu de if / else

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -11,11 +11,26 @@ public class Order {
         System.out.println("3 - végétarien");
         System.out.println("Que souhaitez-vous comme menu ?");
     }
-    /**
-     * Display a selected menu.
-     * @param nbMenu The selected menu.
-     */
-    public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez choisi le menu " + nbMenu);
-    }
-}
+
+     /**
+      * Display a selected menu.
+      * @param nbMenu The selected menu.
+      */
+     public void displaySelectedMenu(int nbMenu) {
+          switch (nbMenu) {
+                case 1:
+                    System.out.println("Vous avez choisi comme menu : poulet");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme menu : boeuf");
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi comme menu : végétarien");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+                    break;
+          }
+     }
+ }
+
